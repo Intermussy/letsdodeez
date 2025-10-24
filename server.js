bc7bb560b60e755f1c2f569d32e93807a0094e6e
musyaffa.satrio <falco4ueki@gmail.com>
2025-10-24 10:37:07 +0700

    another adjustment....

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3000;
-server.listen(port, ()=> {
-    console.log(`JSON Server is running on port ${port}`);
-});
+module.exports = server;
 
 process.on("uncaughtException", console.error);
 process.on("unhandledRejection", console.error);
